6a775e4bc601ffc625ccbc4007eed5609c4bbbdc
nsbendre25 <nihar.bendre@my.utsa.edu>
2019-10-17 15:19:00 -0500

    Initial Commit

---
+++ keras_fgsm_train.py
@@ -0,0 +1,41 @@
+import foolbox
+from foolbox.models import KerasModel
+from foolbox.attacks import LBFGSAttack
+from foolbox.criteria import TargetClassProbability, Misclassification
+import numpy as np
+import keras
+from keras.models import load_model
+import matplotlib.pyplot as plt
+
+
+kmodel = load_model('./LeNet.h5')
+preprocessing = (np.array([104, 116, 123]), 1)
+fmodel = KerasModel(kmodel, bounds=(0, 255))
+attack = LBFGSAttack(model=fmodel, criterion=Misclassification())
+
+
+adversarial_imgs = []
+adversarial_labels =[]
+# adversarial_imgs = np.asarray(adversarial_imgs)
+# adversarial_labels = np.asarray(adversarial_labels)
+# print(type(adversarial_imgs))
+img_temp = np.load('./mnist_pure/x_train.npy')
+# print(img_temp.shape)
+img_temp = np.asarray(img_temp, dtype=np.float32)
+# print(img_temp[0].shape)
+label_temp = np.load('./mnist_pure/y_train.npy')
+label_temp= np.asarray(label_temp, dtype=np.float32)
+
+for i in range(0,60000):
+    adversarial = attack(img_temp[i], label_temp[i])
+    adversarial_imgs.append(adversarial)
+    adv_labels = np.argmax(fmodel.predictions(adversarial))
+    adversarial_labels.append(adv_labels)
+    print(np.array(adversarial_imgs).shape, np.array(adversarial_labels).shape, 'Actual Label: {}, Adversarial Label: {}'.format(label_temp[i], adv_labels))
+
+
+adversarial_imgs = np.asarray(adversarial_imgs)
+adversarial_labels = np.asarray(adversarial_labels)
+
+np.save('./adv_imgs_train.npy', adversarial_imgs)
+np.save('./adv_labels.npy',adversarial_labels)
